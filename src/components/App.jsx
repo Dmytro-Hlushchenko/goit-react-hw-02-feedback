c35f7ffa45518300981b83d8eacf6b4f78274123
Dmytro <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-06-14 10:44:45 +0300

    1-step-Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
-import FeedButtons from "./FeedButtons/";
+import FeedbackOptions from "./FeedbackOptions";
 import Stats from "./FeedStatistics";
 import React, { Component } from "react";
+import { Section } from "./App.styled";
 import PropTypes from 'prop-types';
 
 export class App extends Component {
@@ -9,18 +10,39 @@ export class App extends Component {
     good: 0,
     neutral: 0,
     bad: 0
+  }  
+
+  onLeaveFeedbackGood = (e) => {
+    this.setState((prevState) => ({ good: prevState.good += 1 }))
   }
-        
+  onLeaveFeedbackNeutral = (e) => {
+    this.setState((prevState) => ({ neutral: prevState.neutral += 1 }))
+  }
+  onLeaveFeedbackBad = (e) => {
+    this.setState((prevState) => ({ bad: prevState.bad += 1 }))
+  }
+          
   render() {
+    const { good, neutral, bad } = this.state;
       return (
         <>
-          <h1>Please leave feedback</h1>
-          <FeedButtons />
-          <h2>Statistics</h2>
-          <Stats good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad }
-          />
+          <Section title="Please leave feedback">
+            <h1>Please leave feedback</h1>
+            <FeedbackOptions 
+              options={this.state}
+              onLeaveFeedbackGood={this.onLeaveFeedbackGood}
+              onLeaveFeedbackNeutral={this.onLeaveFeedbackNeutral}
+              onLeaveFeedbackBad={this.onLeaveFeedbackBad}
+            />
+          </Section>
+          <Section title="Statistics">
+            <h2>Statistics</h2>
+            <Stats
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              />
+          </Section>
         </>
       );
     };
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+
+export default function FeedbackOptions({ options, onLeaveFeedbackGood, onLeaveFeedbackNeutral, onLeaveFeedbackBad }) {
+        
+   return (
+        <>
+            <button onClick={onLeaveFeedbackGood} type="button" id = "good" >Good</button>
+            <button onClick={onLeaveFeedbackNeutral} type="button" id = "neutural" >Neutural</button>
+            <button onClick={onLeaveFeedbackBad } type="button" id = "bad">Bad</button>
+        </>
+    )
+};
+
+   
